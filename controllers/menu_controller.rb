39418bb51a17ce8e3047eeb91ee093730fcf28be
ejpgraham <ejpgraham@gmail.com>
2017-09-01 17:14:54 -0400

    Add search and sub menu functions

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -88,10 +88,36 @@ class MenuController
     end
 
     def search_entries
+      print "Search by name:"
+      name = gets.chomp
+      match = address_book.binary_search(name)
+      system "clear"
 
+      if match
+        puts match.to_s
+        search_submenu(match)
+      else
+        puts "No entry with #{name} found"
     end
 
     def read_csv
+      print "Enter CSV file to import: "
+      file_name = gets.chomp
+
+      if file_name.empty?
+        system "clear"
+        puts "no CSV file read"
+        main_menu
+      end
+
+      begin
+        entry_count = address_book.import_from_csv(file_name).count
+        system "clear"
+        puts "#{entry_count} new entries added from #{file_name}"
+      rescue
+        puts "#{file_name} is not a valid CSV file"
+        read_csv
+      end
     end
 
     def entry_submenu(entry)
@@ -104,7 +130,10 @@ class MenuController
       case selection
         when "n"
         when "d"
+          delete_entry(entry)
         when "e"
+          edit_entry(entry)
+          entry_submenu(entry)
         when "m"
           system "clear"
           main_menu
@@ -113,5 +142,48 @@ class MenuController
           puts "#{selection} is not a valid input"
           entry_submenu(entry)
       end
+    end
+
+    def search_submenu(entry)
+      puts "\nd - delete entry"
+      puts "e - edit entry"
+      puts "m - return to main menu"
+
+      selection = gets.chomp
+
+      case selection
+        when "d"
+          delete_entry(entry)
+        when "e"
+          edit_entry(entry)
+        when "m"
+          main_menu
+        else
+          system "clear"
+          "#{selection} is not a valid input"
+          search_submenu(entry)  
+      end
+    end
+
+    def delete_entry(entry)
+      address_book.entries.delete(entry)
+      puts "#{entry} has been deleted."
+    end
+
+    def edit_entry(entry)
+      puts "Update name:"
+      updated_name = gets.chomp
+      puts "Update phone number:"
+      updated_number = gets.chomp
+      puts "Update email address:"
+      updated_email = gets.chomp
+
+      entry.name = updated_name unless updated_name.empty?
+      entry.phone_number = updated_number unless updated_number.empty?
+      entry.email = updated_email unless updated_email.empty?
+
+      puts "Updated entry:"
+      puts entry
+
     end
 end
